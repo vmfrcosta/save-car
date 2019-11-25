8315cc6467f96eb81917d73ae6501642a69e667d
vinicius <vmfrcosta@gmail.com>
2019-11-25 16:19:47 -0300

    added visible and winch to users, also changed stuff to portuguese

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_175357) do
+ActiveRecord::Schema.define(version: 2019_11_25_191904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,8 @@ ActiveRecord::Schema.define(version: 2019_11_25_175357) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.boolean "winch"
+    t.boolean "visible"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
