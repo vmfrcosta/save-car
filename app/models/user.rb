cc823c77121cd64b1e12bf1b8a0e432b4dd2b4e4
vinicius <vmfrcosta@gmail.com>
2019-11-25 15:04:41 -0300

    added first and last name to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :winches
+  has_many :reviews
+  has_many :cars
+  has_many :photos
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_171730) do
+ActiveRecord::Schema.define(version: 2019_11_25_175357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "brand"
+    t.string "model"
+    t.string "plate"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "winch_id"
+    t.integer "rating"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+    t.index ["winch_id"], name: "index_reviews_on_winch_id"
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.bigint "winch_id"
+    t.bigint "car_id"
+    t.string "description"
+    t.datetime "arrival_time"
+    t.datetime "delivery_time"
+    t.string "status"
+    t.float "win_init_lat"
+    t.float "win_init_long"
+    t.float "car_lat"
+    t.float "car_long"
+    t.float "dest_lat"
+    t.float "dest_long"
+    t.integer "total_distance"
+    t.integer "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_trips_on_car_id"
+    t.index ["winch_id"], name: "index_trips_on_winch_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +74,31 @@ ActiveRecord::Schema.define(version: 2019_11_25_171730) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "winches", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "brand"
+    t.string "type"
+    t.string "plate"
+    t.integer "price_per_km"
+    t.float "win_lat"
+    t.float "win_long"
+    t.boolean "on_duty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_winches_on_user_id"
+  end
+
+  add_foreign_key "cars", "users"
+  add_foreign_key "photos", "users"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "reviews", "winches"
+  add_foreign_key "trips", "cars"
+  add_foreign_key "trips", "winches"
+  add_foreign_key "winches", "users"
 end
