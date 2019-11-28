72c7e1a2716678434a81007754224ed15cf8899f
Rhaniel Valle <rhanielvalle@gmail.com>
2019-11-28 13:50:42 -0300

    pre maps configuration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,12 @@ import { getCarLocation } from '../components/location_notification';
 import { setCarLocation } from '../components/location_notification';
 import { initSelect2 } from '../components/init_select2';
 
-initSelect2();
+// initSelect2();
 
 // In your Javascript (external .js resource or <script> tag)
-$(document).ready(function() {
-    $('.js-example-basic-single').select2();
-});
+// $(document).ready(function() {
+//     $('.js-example-basic-single').select2();
+// });
 
 if (document.querySelector('#trips-new')) {
 	getCarLocation();
