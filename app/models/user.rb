27e8a2e6ad238afb7a2001ef08d511600264dc81
Joao Vicente Conte <joao@banfox.com.br>
2019-12-04 12:03:01 -0300

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :cars
 
   has_many :trips
-  mount_uploader :url, PhotoUploader
+  mount_uploader :photo, PhotoUploader
 
   # validates :first_name, presence: true
   # validates :last_name, presence: true
