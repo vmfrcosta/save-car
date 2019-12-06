bd40314c0d2ad834d42904bf2e002396a732accc
Rhaniel Valle <rhanielvalle@gmail.com>
2019-12-06 18:40:27 -0300

    front end que só da trampo

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   has_many :cars
 
   has_many :trips
+  has_many :trip_requests
   mount_uploader :photo, PhotoUploader
 
   # validates :first_name, presence: true
