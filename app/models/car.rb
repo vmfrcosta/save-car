0730c014c00e836ebab27e7eaf6379857621a6e1
carlos silva <carlosantonio.dsilva@gmail.com>
2019-12-06 11:15:36 -0300

    correcao da validacao do car

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,5 +4,5 @@ class Car < ApplicationRecord
   validates :model, presence: true
   validates :plate, presence: true
   validates :car_type, presence: true
-  validates :plate, presence: true, format: { with: /[A-Z]{3}-?\d[A-Z|0-9]\d{2}s/i, message: "-(Placa Invalida)" }, uniqueness: true
+  validates :plate, presence: true, format: { with: /[A-Z]{3}-?\d[A-Z|0-9]\d{2}/i, message: "-(Placa Invalida)" }, uniqueness: true
 end
